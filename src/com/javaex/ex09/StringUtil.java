798fcb420ab1e19d7c959546faba09ac621e35a1
youdaun <yjyou960@naver.com>
2021-11-26 17:12:34 +0900

    9번예제수정

--- src/com/javaex/ex09/StringUtil.java
+++ src/com/javaex/ex09/StringUtil.java
@@ -5,7 +5,11 @@ public class StringUtil {
     public static String concatString(String[] strArray){
        
         //메소드 내용작성
-    	String sum = strArray[0] + strArray[1] + strArray[2];
+    	String sum = "";
+    	
+    	for(int i=0; i<strArray.length; i++) {
+    		sum = sum + strArray[i];
+    	}
         return sum;
     }
 
